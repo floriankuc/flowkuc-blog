7cbc3f76c03495d950cec4e274f112763e81b806
Florian Kuc <florian.kuc@gmail.com>
2021-11-23 23:42:15 +0100

    refactor: container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/react";
+import { jsx, css } from "@emotion/react";
 import { PropsWithChildren, ReactElement, ReactNode } from "react";
 import { mq } from "../utils/breakpoints";
 
@@ -7,28 +7,28 @@ const Container = ({
   children,
 }: PropsWithChildren<ReactNode>): ReactElement => (
   <div
-    css={{
-      display: "flex",
-      flexFlow: "column",
-      margin: "0 auto",
-      width: "90%",
-      minHeight: "100%",
-      [mq.md]: {
-        width: "80%",
-      },
-      [mq.lg]: {
-        width: "70%",
-      },
-      [mq.xl]: {
-        width: "60%",
-      },
-      [mq.xxl]: {
-        width: "50%",
-      },
-      [mq.wow]: {
-        width: "40%",
-      },
-    }}
+    css={css`
+      display: flex;
+      flex-flow: column;
+      margin: 0 auto;
+      width: 90%;
+      min-height: 100%;
+      ${[mq.md]} {
+        width: 80%;
+      }
+      ${[mq.lg]} {
+        width: 70%;
+      }
+      ${[mq.xl]} {
+        width: 60%;
+      }
+      ${[mq.xxl]} {
+        width: 50%;
+      }
+      ${[mq.wow]} {
+        width: 40%;
+      }
+    `}
   >
     {children}
   </div>
