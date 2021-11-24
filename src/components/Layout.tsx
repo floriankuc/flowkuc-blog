8045161bca847279d5d800379542dad743fcd0b7
Florian Kuc <florian.kuc@gmail.com>
2021-11-24 23:57:07 +0100

    refactor: container

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@
 import { jsx, css } from "@emotion/react";
 import React, { PropsWithChildren, ReactElement, ReactNode } from "react";
 import GlobalStyles from "../styles/GlobalStyles";
-import Container from "./Container";
+import Container2 from "./Container2";
 import Footer from "./Footer";
 import Header from "./Header";
 
@@ -11,21 +11,21 @@ const Layout = ({
 }: PropsWithChildren<{ children: ReactNode }>): ReactElement => (
   <>
     <GlobalStyles />
-    <Container>
-      <div style={{ padding: 200 }}>
-        <Header />
-        <main
-          css={css`
-            padding: 50px 0;
-            flex: 1;
-            min-height: calc(100vh - 200px);
-          `}
-        >
-          {children}
-        </main>
-        <Footer />
-      </div>
-    </Container>
+    <Container2>
+      {/* <div style={{ padding: 200 }}> */}
+      <Header />
+      <main
+        css={css`
+          padding: 50px 0;
+          flex: 1;
+          min-height: calc(100vh - 200px);
+        `}
+      >
+        {children}
+      </main>
+      <Footer />
+      {/* </div> */}
+    </Container2>
   </>
 );
 
